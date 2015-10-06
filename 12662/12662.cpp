004fd867bbf48ef56f3ce2f1514b31cdc5a2c6b7
Ricardo Alexis Quiroz Bazan <saquirozbazan@gmail.com>
2015-10-05 19:18:23 -0500

    Good Teacher

--- 12662/12662.cpp
+++ 12662/12662.cpp
@@ -26,59 +26,59 @@ int main()
         ios_base::sync_with_stdio(false);
     #endif
     short n, q, p, index_l, index_r;
-	string L, R;
-	bool left, right;
-	vector<string> student(101);
-
-	cin >> n;
-	for (short i = 0; i < n; ++i)
-		cin >> student[i];
-	cin >> q;
-	while(q--)			
+    string L, R;
+    bool left, right;
+    vector<string> student(101);
+    
+    cin >> n;
+    for (short i = 0; i < n; ++i)
+	cin >> student[i];
+    cin >> q;
+    while(q--)			
+    {
+	cin >> p;
+	p--;
+	if(student[p] != "?") cout << student[p];
+	else
 	{
-		cin >> p;
-		p--;
-		if(student[p] != "?") cout << student[p];
-		else
-		{
-			left = right = false;
-			index_l = index_r = p;	
-			L = R = "";
-			while(!left or !right)
-			{				
-				if(index_r + 1 < n and !right)
-				{
-					index_r++;
-					if (student[index_r] != "?") 
-						right = true;				
-					R += "left of ";
-				}  
-				if(index_l - 1 >= 0 and !left)
-				{
-					index_l--;
-					if (student[index_l] != "?") 
-						left = true;
-					L += "right of ";
-				}				
-				if(index_l == 0) 
+		left = right = false;
+		index_l = index_r = p;	
+		L = R = "";
+		while(!left or !right)
+		{				
+			if(index_r + 1 < n and !right)
+			{
+				index_r++;
+				if (student[index_r] != "?") 
+					right = true;				
+				R += "left of ";
+			}  
+			if(index_l - 1 >= 0 and !left)
+			{
+				index_l--;
+				if (student[index_l] != "?") 
 					left = true;
-				if(index_r == n - 1)
-					right = true;
-			}
-			if ((p - index_l) == (index_r - p) and student[index_r]!="?" and student[index_l]!="?")
-			{
-				cout << "middle of " << student[index_l] << " and " << student[index_r];
-			}
-			else if ((p - index_l >= index_r - p and student[index_r]!="?") or !index_l)
-			{
-				cout << R << student[index_r];
-			}
-			else 
-			{				
-				cout << L << student[index_l];
-			}			
+				L += "right of ";
+			}				
+			if(index_l == 0) 
+				left = true;
+			if(index_r == n - 1)
+				right = true;
 		}
-		cout << '\n';
-	}		
-	return 0;
+		if ((p - index_l) == (index_r - p) and student[index_r]!="?" and student[index_l]!="?")
+		{
+			cout << "middle of " << student[index_l] << " and " << student[index_r];
+		}
+		else if ((p - index_l >= index_r - p and student[index_r]!="?") or !index_l)
+		{
+			cout << R << student[index_r];
+		}
+		else 
+		{				
+			cout << L << student[index_l];
+		}			
+	}
+	cout << '\n';
+    }		
+    return 0;
 }
